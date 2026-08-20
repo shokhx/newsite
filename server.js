d03b699975be432772b0025d9c87b4412f44401d
shokhx <shokhgoog2007@gmail.com>
2026-08-20 13:00:05 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://github.io',
+    credentials: true
+}));
+
 app.use(express.json());
 
 // Подключение к MongoDB
